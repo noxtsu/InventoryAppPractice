d6ec0e8e7a2aa5b24f9c38e3eb52c67a6599fb53
ryota.nozu <kuraraiban29@gmail.com>
2023-07-10 00:41:39 +0900

    activityの編集

--- app/src/main/java/com/example/inventoryapppractice/MainActivity.kt
+++ app/src/main/java/com/example/inventoryapppractice/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.inventoryapppractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
         setContentView(R.layout.activity_main)
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(this, navController)
     }
+
+    /**
+     * Handle navigation when thr user choose Up from the action bar.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
